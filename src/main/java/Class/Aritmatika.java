65cc5f66be45472f98c813d0fa69e573593496b6
evlynngelica <evlyn.angelica@mhs.itenas.ac.id>
2022-12-16 08:32:37 +0700

    ini adalah inisialisasi projek aritmatika

---
+++ src/main/java/Class/Aritmatika.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Aritmatika {
+    private double bilSatu;
+    private double bilDua;
+
+    public Aritmatika(double bilSatu, double bilDua) {
+        this.bilSatu = bilSatu;
+        this.bilDua = bilDua;
+    }
+
+    public double getBilSatu() {
+        return bilSatu;
+    }
+
+    public void setBilSatu(double bilSatu) {
+        this.bilSatu = bilSatu;
+    }
+
+    public double getBilDua() {
+        return bilDua;
+    }
+
+    public void setBilDua(double bilDua) {
+        this.bilDua = bilDua;
+    }
+    
+    //penjumlahan 
+    public double penjumlahan(){
+        return 0;
+    }
+    
+    //pengurangan
+    public double pengurangan(){
+        return 0;
+    }
+    
+    //perkalian 
+    public double perkalian(){
+        return 0;
+    }
+    
+    //pembagian 
+    public double pembagian(){
+        return 0;
+    }
+    
+}
